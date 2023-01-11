f654212ca3c54338c0b8c02959c0880c797bb467
HP <HP@DESKTOP-5P6PFIH>
2023-01-11 14:20:02 +0530

    my second program

--- freestyleproject/src/practice/ClassB.java
+++ freestyleproject/src/practice/ClassB.java
@@ -1,8 +1,9 @@
 package practice;
 
 public class ClassB {
-  public static void main(String[] args) {
-	System.out.print("Bye world");
-}
+
+	public static void main(String[] args) {
+System.out.println("Bye world");
+	}
 
 }
