b04d8d01840c8a186aca0974f29871b7da51d959
pooja7721 <122433016+pooja7721@users.noreply.github.com>
2023-01-11 15:05:05 +0530

    Update ClassB.java

--- freestyleproject/src/practice/ClassB.java
+++ freestyleproject/src/practice/ClassB.java
@@ -3,7 +3,7 @@ package practice;
 public class ClassB {
 
 	public static void main(String[] args) {
-System.out.println("Bye world");
+System.out.println("Bye world, ww will meet tomorrow");
 	}
 
 }
